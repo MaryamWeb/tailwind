18849f292e7364c7165e29b5f8af8c6cd2f5392b
unknown <maryam_lfc@hotmail.com>
2021-07-06 16:46:08 +0300

    project coming soon

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@ module.exports = {
 	purge: [],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {}
+		extend: {
+			backgroundImage: (theme) => ({
+				hero: "url('./images/bg-pattern-desktop.svg')",
+				'desktop-right': "url('./images/hero-desktop.jpg')"
+			}),
+			fontFamily: {
+				body: [ 'Josefin+Sans' ]
+			},
+			colors: {
+				'desaturated-red': 'hsl(0, 36%, 70%)',
+				'soft-red': 'hsl(0, 93%, 68%)'
+			},
+			letterSpacing: {
+				heading: '0.2em'
+			},
+			flex: {
+				'6': '0 0 60%'
+			}
+		}
 	},
 	variants: {
 		extend: {}
