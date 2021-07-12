8a928bac0154e50e27c45554ecca0ad5738f070e
unknown <maryam_lfc@hotmail.com>
2021-07-12 18:10:40 +0300

    Content and style

--- Easybank/tailwind.config.js
+++ Easybank/tailwind.config.js
@@ -1,11 +1,26 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	purge: [],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			backgroundImage: (theme) => ({
+				'bg-lg': "url('./images/bg-intro-desktop.svg')",
+				'bg-sm': "url('./images/bg-intro-mobile.svg')",
+				'bg-phones': "url('./images/image-mockups.png')"
+			}),
+			fontFamily: {
+				Publicsans: [ 'Public+Sans' ]
+			},
+			colors: {
+				'primary-light': '#fafafa',
+				'primary-dark': '#f4f5f7',
+				'lime-green': 'hsl(136, 65%, 51%)',
+				'bright-cyan': 'hsl(192, 70%, 51%)'
+			}
+		}
+	},
+	variants: {
+		extend: {}
+	},
+	plugins: []
+};
