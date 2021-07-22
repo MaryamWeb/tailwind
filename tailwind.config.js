48b4ee3270f440cf2b434b64335ef734114e8b5f
unknown <maryam_lfc@hotmail.com>
2021-07-22 12:39:13 +0300

    Style homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	purge: [],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			colors: {
+				'primary-pink': 'hsl(0, 80%, 86%)'
+			}
+		}
+	},
+	variants: {
+		extend: {}
+	},
+	plugins: []
+};
